99de5ca4b65b7f5b6c533804ef77110c00cec2e3
arifyunando <arifyunando@gmail.com>
2023-08-03 11:37:02 +0200

    💡 reset progress

--- tutorial/Step2/MathFunctions/MathFunctions.cxx
+++ tutorial/Step2/MathFunctions/MathFunctions.cxx
@@ -1,24 +1,15 @@
 #include "MathFunctions.h"
-#include "stdio.h"
+
 // TODO 11: include cmath
-#include <cmath>
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
-
-#ifdef USE_MYMATH
-  #include "mysqrt.h"
-#endif
+#include "mysqrt.h"
 
 namespace mathfunctions {
 double sqrt(double x)
 {
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
-  printf("This is tutorial/Step2 Math Functions\n");
-  #ifdef USE_MYMATH
-    return detail::mysqrt(x);
-  #else
-    return std::sqrt(x);
-  #endif
+  return detail::mysqrt(x);
 }
 }
