aeb0a196821e83d4d5c63874295903982b1ebb94
arifyunando <arifyunando@gmail.com>
2023-08-03 13:20:24 +0200

    🛠️ finished exercise 3

--- tutorial/Step1/tutorial.cxx
+++ tutorial/Step1/tutorial.cxx
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
   if (argc < 2) {
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
-    std::cout PROJECT_NAME << ' Version : ' << Tutorial_VERSION_MAJOR << '.' << Tutorial_VERSION_MINOR
+    std::cout << PROJECT_NAME << " Version : " << Tutorial_VERSION_MAJOR << '.' << Tutorial_VERSION_MINOR << '\n';
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
